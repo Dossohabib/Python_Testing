8a3e0c3d547864ef530f2bc8e40b8a3bf7e3570b
Dossohabib <dossohabib90@gmail.com>
2024-02-12 10:51:50 +0000

    ERREUR LORS DE LA CONNEXION

---
+++ test/tests_unitaire/tests_connexion.py
@@ -0,0 +1,35 @@
+import unittest
+from flask import Flask, url_for
+from app import app
+
+class TestValidEmail(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_valid_email_returns_ok(self):
+        response = self.app.post('/showSummary', data={'email': 'john@simplylift.co'})
+        self.assertEqual(response.status_code, 200)  # Succès
+        self.assertIn(b'Welcome', response.data)
+
+class TestInvalidEmail(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_invalid_email_returns_404(self):
+        response = self.app.post('/showSummary', data={'email': 'invalid@example.com'})
+        self.assertEqual(response.status_code, 302)  # Erreur 404
+        
+class TestNoEmail(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_no_email_returns_redirect(self):
+        response = self.app.post('/showSummary', data={})
+        self.assertEqual(response.status_code, 302)  # Redirection
+       
+
+if __name__ == '__main__':
+    unittest.main()
